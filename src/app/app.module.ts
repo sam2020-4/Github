d34d3a24b0cfe75ab47959da55bbff027e315f4c
matata samuel <mattasamuel3@gmail.com>
2020-08-29 04:27:39 +0300

    added user class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitNavbarComponent } from './git-navbar/git-navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GitNavbarComponent
+    GitNavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
